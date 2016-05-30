907f516f03bd4ec79a57d091abdc4d29a7fb1c81
qilin <qi138138lin@163.com>
2016-05-30 22:41:00 +0800

    修改抓取文章脚本

--- spider/article.py
+++ spider/article.py
@@ -6,6 +6,8 @@ import time
 import os
 import hashlib
 import sys
+import MySQLdb
+import json
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -29,10 +31,22 @@ def output_html(title, content):
     fout.write("</body></html>")
 
 def downloadImg(html):
-
+    return_data = []
     soup = BeautifulSoup(html, "html.parser", from_encoding="utf-8")
     summary_node = soup.find(name="div", attrs={"class": "article-content"})
+
+    if summary_node is None:
+        print "抓取不到数据,或您输入地址有误"
+        os._exit(0)
+
     summary_title = soup.find(name='h1', attrs={"class":"title"}).getText()
+    summary_tag = soup.find(name='ul', attrs={"class":"tag-list"}).findAll('a')
+
+    tag_list = []
+    for tag in summary_tag:
+         tag_list.append(tag.getText())
+
+    summary_tag = ",".join(tag_list)
 
     data = summary_node.findAll('img')
     imglist = {}
@@ -50,12 +64,13 @@ def downloadImg(html):
 
     md = hashlib.md5()
 
+    summary_url = {}
     for (key,imgurl) in imglist.items():
         md5_str = md.update(imgurl)
         md5_str = md.hexdigest()
         target = picpath+'/%s.jpg' % md5_str
         orgin_url  = "http://localhost/"+foldername+'/'+md5_str+'.jpg'
-
+        summary_url[imgurl] = foldername+'/'+md5_str+'.jpg'
 
         summary_node = str(summary_node).replace(imgurl, orgin_url)
         summary_node = summary_node.replace('onerror="javascript:errorimg.call(this);"', '')
@@ -63,15 +78,36 @@ def downloadImg(html):
         print 'Downloading image to location: ' + target + '\nurl=' + imgurl
         image = urllib.urlretrieve(imgurl, target, schedule)
 
+    return_data = [summary_node,summary_title,summary_tag,json.dumps(summary_url),int(time.time())]
     output_html(summary_title, summary_node)
-    return image
 
-  
+    return return_data
+
+def insert_data(data):
+    db = MySQLdb.connect("localhost","root", "root@123", "test")
+    cursor = db.cursor()
+    sql = "insert into spider (`content`,`title`,`tag`,`img_url`,`create_time`,`url`,`source`) " \
+          "values (%s,%s,%s,%s,%s,%s,%s)"
+
+    cursor.executemany(sql, [data])
+    return db.commit()
+
+def get_domain(url):
+    proto, rest = urllib.splittype(url)
+    res, rest = urllib.splithost(rest)
+    return res
+
 if __name__ == '__main__':
+    url = "http://toutiao.com/a6290391209140109569/"
+    html = getHtml(url)
 
-    html = getHtml("http://toutiao.com/a6285980631693459714/")
+    return_data = downloadImg(html)
 
-    downloadImg(html)
+    return_data.append(url)
+    return_data.append(get_domain(url))
+
+    print insert_data(tuple(return_data))
+
     print "Download has finished."
 
 
